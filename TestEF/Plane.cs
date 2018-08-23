d06d2a89b741869c96abb5328d6d0077baeb799b
Slavche.proger <slavche001@gmail.com>
2018-08-23 20:29:34 +0300

    Fixed exception with empty class constructors

--- TestEF/Plane.cs
+++ TestEF/Plane.cs
@@ -5,6 +5,7 @@
         public string AviaComp { get; set; }
         public int AmountOfTurb { get; set; }
 
+        public Plane() : this(0) { }
         public Plane(int id) : this("def. aviaComp", 0, 0, 0.0, 0, id) { }
         public Plane(string aviaComp, int amountOfTurb, int speed, double fuelConsum, int costOfMaintain, int id)
             : base(speed, fuelConsum, costOfMaintain, id)
