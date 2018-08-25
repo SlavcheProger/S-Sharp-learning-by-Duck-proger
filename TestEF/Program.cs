45a8da6a5ebd27ae082a9722f96095a2ee1b6738
что угодно <goowillprevail@gmail.com>
2018-08-25 13:23:49 +0700

    sample text

--- TestEF/Program.cs
+++ TestEF/Program.cs
@@ -7,7 +7,6 @@ namespace TestEF
     public class Program
     {
         public static DataBase DB;
-        // теперь метод Main выглядит, как должен выглядеть :)
         static void Main(string[] args)
         {
             try
@@ -17,44 +16,29 @@ namespace TestEF
             }
             catch (Exception exception)
             {
-                Log(exception);
-            }
-            finally
-            {
-                Console.Write("Press any key to quit: ");
-                Console.ReadKey();
+                ConsoleLog(4, exception.ToString());
             }
         }
         // console color output
         #region
-        //for errors
-        public static void Log(Exception exception)
+        public static void ConsoleLog(int clr, string str)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(exception.ToString());
-            Console.ResetColor();
-            Console.WriteLine();
-        }
-        //basic
-        public static void CLog(string str)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(str);
-            Console.ResetColor();
-            Console.WriteLine();
-        }
-        //for cars
-        public static void CarLog(string str)
-        {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine(str);
-            Console.ResetColor();
-            Console.WriteLine();
-        }
-        //for planes
-        public static void PlaneLog(string str)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
+            switch (clr)
+            {
+	        
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                break;
+                case 4:
+                     Console.ForegroundColor = ConsoleColor.Green;                    
+                break;
+	        }
             Console.WriteLine(str);
             Console.ResetColor();
             Console.WriteLine();
@@ -72,10 +56,10 @@ namespace TestEF
             }
             catch (Exception exception)
             {
-                Log(exception);
+                ConsoleLog(4, exception.ToString());
                 if (!File.Exists(@"..\..\DB\DataBase.json"))
                 {
-                    CLog("Data Base missing, creating a new one");
+                    ConsoleLog(1, "Data Base missing, creating a new one");
                     using (var fs = File.Create(@"..\..\DB\DataBase.json")) { }
                     DB = new DataBase();
                 }
@@ -98,11 +82,11 @@ namespace TestEF
         {
             foreach (var item in db.Cars)
             {
-                CarLog($"Speed: {item.Speed}\nCost of maintain: {item.CostOfMaintain} \nFuel consumation: {item.FuelConsum} \nColor: {item.Color} \nModel: {item.Model} \nId: {item.Id}\ncar\n*");
+                ConsoleLog(2, $"Speed: {item.Speed}\nCost of maintain: {item.CostOfMaintain} \nFuel consumation: {item.FuelConsum} \nColor: {item.Color} \nModel: {item.Model} \nId: {item.Id}\ncar\n*");
             }
             foreach (var item in db.Planes)
             {
-                PlaneLog($"Speed: {item.Speed}\nCost of maintain: {item.CostOfMaintain} \nFuel consumation: {item.FuelConsum} \nAvia company: {item.AviaComp} \nId: {item.Id}\nplane\n*");
+                ConsoleLog(3, $"Speed: {item.Speed}\nCost of maintain: {item.CostOfMaintain} \nFuel consumation: {item.FuelConsum} \nAvia company: {item.AviaComp} \nId: {item.Id}\nplane\n*");
             }
         }
         #endregion
@@ -112,25 +96,25 @@ namespace TestEF
         {
             while (true)
             {
-                CLog("What do you want to change: car or plane?");
+                ConsoleLog(1, "item do you want to change: car or plane?");
                 tryAgain:
-                var what = Console.ReadLine().ToLower();
-                if (what != "car" && what != "plane")
+                var item = Console.ReadLine().ToLower();
+                if (item != "car" && item != "plane")
                 {
-                    CLog("Please, enter \"car\" or \"plane\"");
+                    ConsoleLog(1, "Please, enter \"car\" or \"plane\"");
                     goto tryAgain;
                 }
 
-                CLog($"1-remove {what} (by ID)\n2-add default {what} \n3-add {what} with parameters");
+                ConsoleLog(1, $"1-remove {item} (by ID)\n2-add default {item} \n3-add {item} with parameters");
 
                 var choice = Choose();
 
-                var id = RandId(what);
+                var id = CreateRandomId(item);
 
-                DBChange(id, what, choice);
+                CommandHandler(id, item, choice);
 
                 ShowDB(DB);
-                CLog("If you want to exit, type 0, or anything else to continue: ");
+                ConsoleLog(1, "If you want to exit, type 0, or anything else to continue: ");
 
                 if (Int32.TryParse(Console.ReadLine(), out int input) && input == 0)
                 {
@@ -139,24 +123,24 @@ namespace TestEF
             }
         }
 
-        public static void DBChange(int id, string what, int choice)
+        public static void CommandHandler(int id, string item, int choice)
         {
             switch (choice)
             {
                 case 1:
-                    RemoveSmth(what);
+                    RemoveItemById(item);
                     break;
                 case 2:
-                    AddSmth(id, what);
+                    AddNewDefaultItem(id, item);
                     break;
                 case 3:
-                    AddCustom(id, what);
+                    AddNewCustomItem(id, item);
                     break;
             }
             WriteDataToFile(DB);
         }
 
-        public static void AddCustom(int id, string what)
+        public static void AddNewCustomItem(int id, string item)
         {
             retry:
             try
@@ -167,7 +151,7 @@ namespace TestEF
                 var par2 = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Cost of maintaining: ");
                 var par3 = Convert.ToInt32(Console.ReadLine());
-                if (what == "car")
+                if (item == "car")
                 {
                     Console.Write("Model: ");
                     var cpar1 = Console.ReadLine();
@@ -175,7 +159,7 @@ namespace TestEF
                     var cpar2 = Console.ReadLine();
                     DB.Cars.Add(new Car(cpar1, cpar2, par1, par2, par3, id));
                 }
-                else if (what == "plane")
+                else if (item == "plane")
                 {
                     var ppar1 = Console.ReadLine();
                     Console.Write("Amount of turbines: ");
@@ -185,75 +169,56 @@ namespace TestEF
             }
             catch (Exception exception)
             {
-                CLog("Incorrect format of the last variable, try again");
+                ConsoleLog(1, "Incorrect format of the last variable, try again");
                 goto retry;
             }
         }
 
-        public static void AddSmth(int id, string what)
+        public static void AddNewDefaultItem(int id, string item)
         {
-            if (what == "car")
+            if (item == "car")
             {
                 DB.Cars.Add(new Car(id));
             }
-            else if (what == "plane")
+            else if (item == "plane")
                 DB.Planes.Add(new Plane(id));
         }
 
-        public static void RemoveSmth(string what)
+        public static void RemoveItemById(string item)
         {
             retry:
-            var input = "";
             try
             {
-                if (what == "car" && DB.Cars.Count != 0)
+                if ((item == "car" && DB.Cars.Count != 0) || (item == "plane" && DB.Planes.Count != 0))
                 {
-                    CLog($"Insert {what}`s id, or type \"exit\" to cancel");
-                    input = Console.ReadLine();
-                    if (input != "exit")
-                    {
-                        var id = Convert.ToInt32(input);
-                        var that = DB.Cars.FindIndex(x => x.Id == id);
-                        DB.Cars.RemoveAt(that);
-                    }
-                }
-                else if (what == "plane" && DB.Planes.Count != 0)
-                {
-                    CLog($"Insert {what}`s id, or type \"exit\" to cancel");
-                    input = Console.ReadLine();
-                    if (input != "exit")
-                    {
-                        var id = Convert.ToInt32(input);
-                        var that = DB.Planes.FindIndex(x => x.Id == id);
-                        DB.Planes.RemoveAt(that);
-                    }
+                    TestOnInput(item);
                 }
                 else
                 {
-                    CLog($"Sorry, there is no {what} in DataBase.");
+                    ConsoleLog(1, $"Sorry, there is no {item} in DataBase.");
                 }
             }
             catch (Exception exception)
             {
-                CLog($"Wrong {what}`s id! Please, try again.");
+                ConsoleLog(1, $"Wrong {item}`s id! Please, try again.");
                 goto retry;
             }
         }
         #endregion
         // Misc
         #region
-        public static int RandId(string what)
+        public static int CreateRandomId(string item)
         {
             var rand = new Random();
             var id = rand.Next(0, 1000);
-            if (what == "car")
+            if (item == "car")
             {
                 while (DB.Cars.Exists(x => x.Id == id))
                 {
                     id = rand.Next(0, 1000);
                 }
             }
-            else if (what == "plane")
+            else if (item == "plane")
             {
                 while (DB.Planes.Exists(x => x.Id == id))
                 {
@@ -270,10 +235,30 @@ namespace TestEF
             var result = Int32.TryParse(Console.ReadLine(), out int choice);
             if (!result || choice != 1 && choice != 2 && choice != 3)
             {
-                CLog("Choose 1, 2 or 3");
+                ConsoleLog(1, "Choose 1, 2 or 3");
                 goto theChoice;
             }
             return choice;
+        }
+
+        public static void TestOnInput(string item)
+        {
+            ConsoleLog(1, $"Insert {item}`s id, or type \"exit\" to cancel");
+            var input = Console.ReadLine();
+            if (input != "exit")
+            {
+                var id = Convert.ToInt32(input);
+                if (item == "car")
+                {
+                    var IndexOfTheItem = DB.Cars.FindIndex(x => x.Id == id);
+                    DB.Cars.RemoveAt(IndexOfTheItem);
+                }
+                else if (item == "plane")
+                {
+                    var IndexOfTheItem = DB.Planes.FindIndex(x => x.Id == id);
+                    DB.Planes.RemoveAt(IndexOfTheItem);
+                }
+            }
         }
         #endregion
 
@@ -281,6 +266,6 @@ namespace TestEF
 }
 
 // TODO: основные правки в коде
-// TODO: переделай метод RemoveSmth. тут много повторяющегося кода - исправь это.
+// TODO: переделай метод RemoveItemById. тут много повторяющегося кода - исправь это.
 // TODO: из всех цветных выводов в консоль сделай один метод, который принимает 2 параметра : ошибку и цвет. ну и соответственно в коде это исправь. ну и тогда перенеси его к прочим методам.
 // TODO: сделай НОРМАЛЬНЫЕ названия переменных и методов, чтобы человек, посмотрев на название, МОГ ПОНЯТЬ, за что оно отвечает.
